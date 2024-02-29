6e78694f5e1b1ca5017ca8c13c2cd2a6423e7ae8
alicjapol <alicjaxpoltorak@gmail.com>
2024-02-29 22:08:28 +0100

    Update cat animation

--- src/index.js
+++ src/index.js
@@ -2,62 +2,58 @@ import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
-axios.defaults.headers.common['x-api-key'] =
-  'live_FGH9lasd0oul4RhK4aaXXVevkdqzPmluPpIdqRAPTVq9GAtoohZ7VPmrFhJI1oqr';
+axios.defaults.headers.common['x-api-key'] =  'live_FGH9lasd0oul4RhK4aaXXVevkdqzPmluPpIdqRAPTVq9GAtoohZ7VPmrFhJI1oqr';
 
 const breedSelect = document.querySelector('.breed-select');
-const loader = document.querySelector('.loader');
+const loader = document.querySelector('.loading-cat'); // Zaktualizowane dla nowej animacji kota
 const error = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
 
 breedSelect.style.display = 'none';
+catInfo.style.display = 'none';
 
 fetchBreeds()
   .then(data => {
     const options = data.data;
-
-    loader.style.display = 'block';
+    
+    loader.style.display = 'none';
     error.style.display = 'none';
 
     breedSelect.innerHTML = options
       .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
       .join('');
 
-    loader.style.display = 'none';
+    breedSelect.style.display = 'block';
   })
   .catch(() => {
     loader.style.display = 'none';
     error.style.display = 'block';
-  })
-  .finally(() => {
-    breedSelect.style.display = 'block';
   });
 
 breedSelect.addEventListener('change', event => {
+  const breedId = event.target.value;
+
   error.style.display = 'none';
   loader.style.display = 'block';
   catInfo.style.display = 'none';
 
-  const breed = event.target.value;
-
-  fetchCatByBreed(breed)
+  fetchCatByBreed(breedId)
     .then(data => {
-      const catData = data.data[0].breeds[0];
+      if (data.data.length > 0 && data.data[0].breeds.length > 0) {
+        const catData = data.data[0].breeds[0];
 
-      if (catData) {
         catInfo.innerHTML = `
-          <p>${catData.name}</p>
+          <h4>${catData.name}</h4>
           <p>${catData.description}</p>
           <p>${catData.temperament}</p>
-          <img src='${data.data[0].url}' >`;
-      } else {
-        catInfo.innerHTML = Notiflix.Notify.failure(
-          'No information available for this breed'
-        );
-      }
+          <img src='${data.data[0].url}' alt='${catData.name}'>`;
 
-      loader.style.display = 'none';
-      catInfo.style.display = 'block';
+        loader.style.display = 'none';
+        catInfo.style.display = 'block';
+      } else {
+        Notiflix.Notify.failure('No information available for this breed');
+        loader.style.display = 'none';
+      }
     })
     .catch(() => {
       loader.style.display = 'none';
